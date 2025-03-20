b8d463936a323f4e74cb8ea8d43d31a49cc712b9
NinoApollo <napollo@filamer.edu.ph>
2025-03-20 17:21:28 +0800

    Added Getting Functional

---
+++ Getting Functional/function.js
@@ -0,0 +1,42 @@
+// var dogName = "harley";
+// var dogWeight = 25;
+// if (dogWeight > 20) {
+//     console.log(dogName + " says WOOF WOOF");
+// } else {
+//     console.log(dogName + " says woof woof")
+// }
+
+// var dogName = "bruno";
+// var dogWeight = 27;
+// if (dogWeight > 20) {
+//     console.log(dogName + " says WOOF WOOF");
+// } else {
+//     console.log(dogName + " says woof woof")
+// }
+
+// var dogName = "moscov";
+// var dogWeight = 15;
+// if (dogWeight > 20) {
+//     console.log(dogName + " says WOOF WOOF");
+// } else {
+//     console.log(dogName + " says woof woof")
+// }
+
+// var dogName = "franco";
+// var dogWeight = 10;
+// if (dogWeight > 20) {
+//     console.log(dogName + " says WOOF WOOF");
+// } else {
+//     console.log(dogName + " says woof woof")
+// }
+
+function bark(name, weight) {
+    if (weight > 20) {
+        console.log(name  + " says WOOF WOOF");
+    } else {
+        console.log(name  + " says woof woof");
+    }
+}
+
+bark("Joshua", 25);
+bark("Clyde", 15);
